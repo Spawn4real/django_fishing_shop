0b0774c6eb3a3629454dc8840a3b86b1aaf2ce0f
maxim <m.demchenko2001@gmail.com>
2022-06-14 22:35:59 +0700

    lesson_2

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp.home),
-    path('products/', mainapp.products),
-    path('contacts/', mainapp.contacts)
+    path('', mainapp.main, name='main'),
+    path('products/', mainapp.products, name='products'),
+    path('contacts/', mainapp.contacts, name='contacts'),
 ]
--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -2,14 +2,48 @@ from django.shortcuts import render
 
 # Create your views here.
 
+main_menu = [
+    {'href': '/', 'name': 'Домой'},
+    {'href': '/products', 'name': 'Продукты'},
+    {'href': '/contacts', 'name': 'Контакты'},
+]
+
+links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'hre f': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+]
+
+
 # Контроллер для домашней странциы
-def home(request):
-    return render(request, 'mainapp/home.html')
+def main(request):
+    title = 'Главная'
+    context = {
+        'title': title,
+        'main_menu': main_menu,
+    }
+    return render(request, 'mainapp/main.html', context=context)
+
 
 # Контроллер для страницы с товарами
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Каталог'
+    context = {
+        'title': title,
+        'main_menu':main_menu,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'mainapp/products.html', context=context)
+
 
 # Контроллер для страницы с контактами
 def contacts(request):
-    return render(request, 'mainapp/contacts.html')
+    title = 'Контакты'
+    context = {
+        'title': title,
+        'main_menu': main_menu,
+    }
+    return render(request, 'mainapp/contacts.html', context=context)
